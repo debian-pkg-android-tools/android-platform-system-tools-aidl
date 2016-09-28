950e75a7ef8b2738cd958320c46bac12c21c646f
Kai-Chung Yan (殷啟聰) <seamlikok@gmail.com>
2016-09-28 15:46:07 +0800

    aidl & aidl-cpp build against libbase

--- debian/aidl.mk
+++ debian/aidl.mk
@@ -1,7 +1,8 @@
 NAME = aidl
 SOURCES = main_java.cpp
 LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
-           -L. -laidl-common
+           -L. -laidl-common \
+           -L/usr/lib/$(DEB_HOST_MULTIARCH)/android -lbase
 
 build: $(SOURCES)
 	$(CXX) $^ -o $(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
